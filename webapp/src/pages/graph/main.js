3869b97ec376c0dfc82ad1f55ea3e04a9a5c5b61
berniehuang2008 <berniehuang2008@163.com>
2024-04-01 15:31:36 +0800

    Scale

--- webapp/src/pages/graph/main.js
+++ webapp/src/pages/graph/main.js
@@ -1,12 +1,25 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 function InfBoard() {
+    var ref = useRef(null);
     var [viewPoint, setViewPoint] = useState([0, 0]);   // [x, y]
-    var [viewScale, setViewScale] = useState(1);
+    var [viewScale, setViewScale] = useState(1.0);
     var [canvasSize, setCanvasSize] = useState([0, 0, 0, 0]);    // [x, y, width, height]
+    var [inited, setInited] = useState(false);
+
+    // (helper) refs
+    const viewScaleRef = useRef(viewScale);
+    const viewPointRef = useRef(viewPoint);
+    useEffect(() => {
+        viewScaleRef.current = viewScale;
+        viewPointRef.current = viewPoint;
+    }, [viewScale, viewPoint]);
 
     // when scrollbar scrolls
     function onSbScroll() {
+        if (inited === false)
+            return true;
+
         var sbX = document.querySelector('.vscrollbar-x');
         var sbY = document.querySelector('.vscrollbar-y');
 
@@ -17,16 +30,56 @@ function InfBoard() {
     }
 
     // when mouse wheel scrolls
-    function updateSb(e) {
+    function onWhScroll(e) {
         var sbX = document.querySelector('.vscrollbar-x');
         var sbY = document.querySelector('.vscrollbar-y');
+        var deltaX = e.deltaX;
+        var deltaY = e.deltaY;
 
-        sbX.scrollBy(e.deltaX, 0);
-        sbY.scrollBy(0, e.deltaY);
+        // ctrl: scale
+        if (e.ctrlKey === true) {
+            return onScale(e);
+        }
 
-        setViewPoint([viewPoint[0] + e.deltaX, viewPoint[1] + e.deltaY]);
+        // shift: horizontal scroll
+        if (e.shiftKey === true) {
+            deltaX = e.deltaY;
+            deltaY = e.deltaX;
+        }
+
+        sbX.scrollBy(deltaX, 0);
+        sbY.scrollBy(0, deltaY);
+    }
+
+    // when area scales
+    function onScale(e) {
+        const alpha = 0.005;
+
+        // update scale
+        const oldScale = viewScaleRef.current;
+        const newScale = oldScale * Math.pow(1.1, e.wheelDelta * alpha);
+
+        // update view point
+        var viewPoint = viewPointRef.current;
+        // convert screen-coord to anchor-px-coord
+        const anchorX = -viewPoint[0];
+        const anchorY = -viewPoint[1];
+        var cx = e.clientX - 51;
+        var cy = e.clientY;
+        var dx = (cx - anchorX) * (newScale - oldScale) / oldScale;
+        var dy = (cy - anchorY) * (newScale - oldScale) / oldScale;
+
+        // actuall scale
+        setViewScale(newScale);
+
+        // actuall scroll
+        const sbX = document.querySelector('.vscrollbar-x');
+        const sbY = document.querySelector('.vscrollbar-y');
+        sbX.scrollBy(dx, 0);
+        sbY.scrollBy(0, dy);
 
         // prevent default
+        e.preventDefault();
         return false;
     }
 
@@ -55,9 +108,30 @@ function InfBoard() {
         })
     })).observe(document.querySelector('#anchor'), { childList: true }), []);
 
+    // init
+    useEffect(() => {
+        // init v-scrollbar
+        var sbX = document.querySelector('.vscrollbar-x');
+        var sbY = document.querySelector('.vscrollbar-y');
+        sbX.scrollLeft = -canvasSize[0] + window.innerWidth / 2;
+        sbY.scrollTop = -canvasSize[1] + window.innerHeight / 2;
+
+        // add listeners
+        ref.current.addEventListener('wheel', onWhScroll, { passive: false });
+
+        setInited(true);
+
+        return () => {
+            if (ref.current === null)
+                return;
+
+            ref.current.removeEventListener('wheel', onWhScroll);
+        }
+    }, [])
+
     return (
         <>
-            <div id="InfBoardViewBox" onWheel={updateSb}>
+            <div id="InfBoardViewBox" ref={ref}>
                 <div id="anchor" style={{
                     width: 0,
                     height: 0,
@@ -65,14 +139,14 @@ function InfBoard() {
                     position: 'relative',
                     left: -viewPoint[0],
                     top: -viewPoint[1],
-                    scale: viewScale,
+                    transform: `scale(${viewScale})`,
                 }}></div>
             </div>
             <div className="footbar">
-                <div className="vscrollbar-x" onScroll={onSbScroll}><div style={{ width: (canvasSize[2] + window.innerWidth * 2) / viewScale }}></div></div>
+                <div className="vscrollbar-x" onScroll={onSbScroll}><div className="scroll-block" style={{ width: canvasSize[2] * viewScale }}></div></div>
             </div>
             <div className="rightbar">
-                <div className="vscrollbar-y" onScroll={onSbScroll}><div style={{ height: (canvasSize[3] + window.innerHeight * 2) / viewScale }}></div></div>
+                <div className="vscrollbar-y" onScroll={onSbScroll}><div className="scroll-block" style={{ height: canvasSize[3] * viewScale }}></div></div>
             </div>
         </>
     )
