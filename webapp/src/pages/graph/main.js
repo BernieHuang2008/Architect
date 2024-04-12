61cc804e7cb1b5d722951a9d602f9732d726325c
berniehuang2008 <berniehuang2008@163.com>
2024-04-13 07:23:08 +0800

    fix the auto-shrink board

--- webapp/src/pages/graph/main.js
+++ webapp/src/pages/graph/main.js
@@ -48,11 +48,16 @@ function InfBoard() {
             deltaY = e.deltaX;
         }
 
+        if (deltaX + deltaY < 80)
+            document.querySelector('#anchor').classList.add('no-animation');
+
         sbX.scrollBy(deltaX, 0);
         sbY.scrollBy(0, deltaY);
+
+        document.querySelector('#anchor').classList.remove('no-animation');
     }
 
-    // when area scales
+    // when scales
     function onScale(e) {
         const alpha = 0.005;
 
@@ -70,7 +75,6 @@ function InfBoard() {
         var dx = (cx - anchorX) * (newScale - oldScale) / oldScale;
         var dy = (cy - anchorY) * (newScale - oldScale) / oldScale;
 
-        // TODO
         requestAnimationFrame(() => {
             // actuall scale
             setViewScale(newScale);
@@ -95,31 +99,6 @@ function InfBoard() {
         sbX.scrollLeft = -canvasSize[0] + window.innerWidth / 2;
         sbY.scrollTop = -canvasSize[1] + window.innerHeight / 2;
 
-        // add listeners: when HTML changes
-        (new MutationObserver((mutations) => {
-            mutations.forEach((mutation) => {
-                // add node
-                if (mutation.addedNodes.length > 0) {
-                    mutation.addedNodes.forEach((node) => {
-                        var nx1 = node.offsetLeft;
-                        var ny1 = node.getBoundingClientRect().y;
-                        var nx2 = nx1 + node.getBoundingClientRect().x;
-                        var ny2 = ny1 + node.getBoundingClientRect().y;
-                        console.log(nx1, ny1, nx2, ny2)
-                        // update canvas size
-                        setCanvasSize([
-                            Math.min(canvasSize[0], nx1),
-                            Math.min(canvasSize[1], ny1),
-                            Math.max(canvasSize[2], nx2),
-                            Math.max(canvasSize[3], ny2)
-                        ]);
-                    })
-                }
-                // remove node
-                // for performance reasons, we will update it only when saving into the disk
-            })
-        })).observe(document.querySelector('#anchor'), { childList: true })
-
         // add listeners
         var infBoard = ref.current;
         infBoard.addEventListener('wheel', onWhScroll, { passive: false });
@@ -141,7 +120,7 @@ function InfBoard() {
     return (
         <>
             <div id="InfBoardViewBox" ref={ref}>
-                <Anchor viewPoint={viewPoint} viewScale={viewScale} />
+                <Anchor viewPoint={viewPoint} viewScale={viewScale} canvasSize={canvasSize} setCanvasSize={setCanvasSize} />
             </div>
             <div className="footbar">
                 <div className="vscrollbar x" onScroll={onSbScroll}><div className="scroll-block" style={{ width: canvasSize[2] * viewScale }}></div></div>
--- webapp/src/pages/graph/render_content.js
+++ webapp/src/pages/graph/render_content.js
@@ -1,12 +1,13 @@
+import { useState } from 'react';
 import './board.css';
 
-function NodeContents({ nodes }) {
+function NodeContents({ nodes, choose }) {
     return (
         <div id="anchor-node">
             {Object.keys(nodes).map(id => {
                 let node = nodes[id];
                 return (
-                    <div key={node.id} className="node" style={{ left: node.x, top: node.y }}>
+                    <div key={node.id} className="node" onClick={choose(node.id)} style={{ left: node.x, top: node.y }}>
                         {node.name}
                     </div>
                 );
@@ -15,7 +16,7 @@ function NodeContents({ nodes }) {
     )
 }
 
-function EdgeContents({ nodes, edges }) {
+function EdgeContents({ nodes, edges, choose }) {
     return (
         <div id="anchor-edge">
             {Object.keys(edges).map(id => {
@@ -23,39 +24,66 @@ function EdgeContents({ nodes, edges }) {
                 var src = nodes[edge.src];
                 var dst = nodes[edge.dst];
 
+                var d;
                 switch (edge.curve) {
                     case 'bezier':
-                        return (
-                            <svg key={edge.id} className="edge" style={{ left: src.x, top: src.y }}>
-                                <path d={`M ${src.x} ${src.y} C ${src.x} ${(src.y + dst.y) / 2}, ${dst.x} ${(src.y + dst.y) / 2}, ${dst.x} ${dst.y}`}/>
-                            </svg>
-                        )
+                        d = `M ${src.x} ${src.y} C ${src.x} ${(src.y + dst.y) / 2}, ${dst.x} ${(src.y + dst.y) / 2}, ${dst.x} ${dst.y}`;
+                        break;
                     default:
-                        return null;
+                        d = `M ${src.x} ${src.y} L ${dst.x} ${dst.y}`;
                 }
+
+                return (
+                    <svg key={edge.id} className="edge" style={{ left: src.x, top: src.y }}>
+                        <path className="edge-display" d={d} />
+                        <path className="edge-interaction" onClick={choose(edge.id)} d={d} />
+                    </svg>
+                )
             })}
         </div>
     )
 }
 
-function BoardContent() {
-    var nodes = {
+function BoardContent({ canvasSize, setCanvasSize }) {
+    var [nodes, setNodes] = useState({
         "N-0": { id: "N-0", x: 100, y: 0, name: 'a' },
         "N-1": { id: "N-1", x: 0, y: 100, name: 'b' },
-    };
-    var edges = {
+    });
+    var [edges, setEdges] = useState({
         "E-0": { id: "E-0", src: "N-0", dst: "N-1", name: 'a->b', curve: 'bezier' },
-    };
+    });
+
+    // setTimeout(() => {
+    //     setNodes({"N-0": { id: "N-0", x: 100, y: 0, name: 'a' }, "N-1": { id: "N-1", x: 0, y: 100, name: 'b' }, "N-2": { id: "N-2", x: 200, y: 200, name: 'c' }});
+    // }, 1000);
+
+    // set canvas size
+    var nx1 = canvasSize[0], ny1 = canvasSize[1], nx2 = canvasSize[2], ny2 = canvasSize[3];
+    for (var n in nodes) {
+        nx1 = Math.min(nx1, nodes[n].x);
+        ny1 = Math.min(ny1, nodes[n].y);
+        nx2 = Math.max(nx2, nodes[n].x);
+        ny2 = Math.max(ny2, nodes[n].y);
+    }
+    if (nx1 !== canvasSize[0] || ny1 !== canvasSize[1] || nx2 !== canvasSize[2] || ny2 !== canvasSize[3])   // MUST! otherwise infinite loop
+        setCanvasSize([nx1, ny1, nx2, ny2]);
+
+    function choose(id) {
+        // wrapper
+        return function () {
+            console.log('choose', id);
+        }
+    }
 
     return (
         <>
-            <NodeContents nodes={nodes} />
-            <EdgeContents nodes={nodes} edges={edges} />
+            <NodeContents choose={choose} nodes={nodes} />
+            <EdgeContents choose={choose} nodes={nodes} edges={edges} />
         </>
     )
 }
 
-function Anchor({ viewPoint, viewScale }) {
+function Anchor({ viewPoint, viewScale, canvasSize, setCanvasSize }) {
     return (
         <div id="anchor" style={{
             // position & scale
@@ -63,7 +91,7 @@ function Anchor({ viewPoint, viewScale }) {
             top: -viewPoint[1],
             transform: `scale(${viewScale})`,
         }}>
-            <BoardContent />
+            <BoardContent canvasSize={canvasSize} setCanvasSize={setCanvasSize} />
         </div>
     )
 }
